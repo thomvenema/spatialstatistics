01c8cf2adcc3fb7d8e2ddec0edf64a9a42b47810
thomvenema <twm.venema@gmail.com>
2022-03-29 11:41:47 +0200

    count toegevoegd tellen aantal gebruikte bureaus

--- opkomst_per_stembureau.R
+++ opkomst_per_stembureau.R
@@ -15,11 +15,12 @@ for (i in plaatsen){
   }
 }
 
-
+aantal_gebruikt <- 0
 df_total = data.frame()
 for (k in 1:length(folderfile_l)){
   data_verkiezing <- read.csv(folderfile_l[k], header=FALSE, sep=";")
   if (data_verkiezing[8,1] == "Postcode") {
+    aantal_gebruikt <- aantal_gebruikt + 1
     data_verkiezing_test <- data_verkiezing %>% slice(c(6,8,9,14))
     data_verkiezing_test <- as.data.frame(t(data_verkiezing_test)) %>% slice(-c(1:5))
     data_verkiezing_test$V5 <- as.numeric(data_verkiezing_test$V4)/as.numeric(data_verkiezing_test$V3)
