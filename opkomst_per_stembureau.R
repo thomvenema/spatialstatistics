ecd530fb91368bf2fd2e8ad96c0de689d667aaf0
thomvenema <twm.venema@gmail.com>
2022-03-29 11:52:13 +0200

    Everything within github folder

--- opkomst_per_stembureau.R
+++ opkomst_per_stembureau.R
@@ -1,6 +1,8 @@
 library(dplyr)
-
-setwd("~/Downloads/GR2022_osv4-3_2022-03-23T21.36/")
+library(rstudioapi)
+setwd(dirname(getActiveDocumentContext()$path))
+setwd("data/")  
+getwd()      
 
 plaatsen <- list.files(pattern="")
 folderfile <- ""
@@ -29,10 +31,7 @@ for (k in 1:length(folderfile_l)){
 }
 colnames(df_total) <- c("Street/Name", "Zipcode", "Invited", "Turnout", "Turnout percentage")
 
-#data_verkiezing <- osv4.3_telling_gr2022_utrecht
-#data_verkiezing_test <- data_verkiezing %>% slice(c(6,8,9,14))
-#data_verkiezing_test <- as.data.frame(t(data_verkiezing_test)) %>% slice(-c(1:5))
-#data_verkiezing_test$V5 <- as.numeric(data_verkiezing_test$V4)/as.numeric(data_verkiezing_test$V3)
 
-#colnames(data_verkiezing_test) <- c("Street", "Zipcode", "Invited", "Turnout", "Turnout percentage")
-                          
+
+write.csv(df_total,"pollingstations.csv", row.names = FALSE)
+
